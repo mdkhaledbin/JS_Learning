0c2c8e62cbe8d8f13f6e1038c649e739df269d69
mdkhaledbin <mdkhaledbin1221@gmail.com>
2024-12-24 08:44:19 +0600

    datatypes

---
+++ 01_basic/01_DataTypes.js
@@ -0,0 +1,27 @@
+//"use strict";//treat all js code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+console.log(3 + 3) //code readability should be high
+console.log("Khaled")
+
+
+let name = "Khaled"
+let age = 22
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigInt
+// string 
+// boolean
+// null => standalone value => object
+// undefined 
+// symbol => uniqueness 
+
+// object 
+
+console.log(typeof "khaled")
+console.log(typeof age);
+console.log(typeof null)
+console.log(typeof undefined)
+
