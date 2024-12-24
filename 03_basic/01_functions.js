100708634fb1173b82a2a6dad7d6241f339a3fdc
mdkhaledbin <mdkhaledbin1221@gmail.com>
2024-12-24 21:49:39 +0600

    Function part 2

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -40,5 +40,38 @@ function loginUserMessage(username = "Sam"){
     return `${username} just logged in.`
 }
 
-console.log(loginUserMessage("Khaled"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage("Khaled"))
+// console.log(loginUserMessage())
+
+
+function calculateCartPrice(...num1){ //rest operator ...
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+    username: "Hitesh",
+    price: 99
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+handleObject(
+    {
+        username: "Sam",
+        price: 399
+    }
+)
+
+const myNewArray = [100, 200, 300, 400]
+
+function retrunSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(retrunSecondValue(myNewArray))
+console.log(retrunSecondValue([200, 400 , 600, 100]))
+
